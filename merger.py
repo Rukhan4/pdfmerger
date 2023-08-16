87de54b5df34f40d720322a40d7fb9fd6d05cf21
Surur Khan <surur.rkhan@gmail.com>
2023-08-16 11:05:58 -0400

    input custom save file name

--- merger.py
+++ merger.py
@@ -10,6 +10,8 @@ merger = PyPDF2.PdfMerger()
 download_dir = "/mnt/c/Users/18687/Downloads/"
 # print(download_dir)
 
+filename = str(input("Enter save file name: "))
+
 
 def merge_curdir_pdfs():
     '''
@@ -19,7 +21,7 @@ def merge_curdir_pdfs():
     for item in os.listdir(os.curdir):
         if item.endswith('.pdf'):
             merger.append(item)
-    merger.write(download_dir + 'curdir_merge.pdf')
+    merger.write(download_dir + filename + '.pdf')
 
 
 def merge_cmdline_pdfs(pdf_list):
@@ -30,7 +32,7 @@ def merge_cmdline_pdfs(pdf_list):
     '''
     for pdf in pdf_list:
         merger.append(pdf)
-    merger.write(download_dir + 'cmdline_merge.pdf')
+    merger.write(download_dir + filename + '.pdf')
 
 
 def main():
@@ -39,12 +41,15 @@ def main():
 
     if len(sys.argv) == 1:  # no command line arguments
         merge_curdir_pdfs()
+        print("PDFs merged successfully!")
         sys.exit(0)
     else:  # command line arguments present
         pdf_list = sys.argv[1:]
         # Cross-platform compatibility between wsl and windows
         wsl_pdf_list = [pdf.replace("C:", "/mnt/c").replace("\\", "/") for pdf in pdf_list]
         merge_cmdline_pdfs(wsl_pdf_list)
+        print("PDFs merged successfully!")
+        sys.exit(0)
 
 
 if __name__ == '__main__':
